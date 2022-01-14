282dccfd7032ac5844ead23eac34c80f9937b2a5
Hannah McCullough <hmac091@gmail.com>
2022-01-14 16:46:35 -0700

    commit tree 32e934df620b43625d0bd943bfb6915a58b858fe

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,10 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Dancer
+    include Dance
+    extend MetaDancing
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+end
